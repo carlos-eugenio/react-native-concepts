540a4f96f90a8a895fb3704d8ed12d157a1237b0
carlos-eugenio <carlos-eugenio@live.com>
2020-10-13 08:58:24 -0300

    feature completed: show, create and like repositories from app; connect to backend using api

--- src/App.js
+++ src/App.js
@@ -1,55 +1,71 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
+import { SafeAreaView, View, FlatList, Text, StatusBar, StyleSheet, TouchableOpacity, } from "react-native";
 
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  Text,
-  StatusBar,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+import api from './services/api';
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=> {
+      api.get('repositories').then(response => {
+          setRepositories(response.data);
+      });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+      const response = await api.post(`repositories/${id}/like`);
+      const likedRepository = response.data;
+      const repositoriesUpdated = repositories.map(repository => {
+          if (repository.id === id){
+              return likedRepository;
+          }
+          else {
+              return repository;
+          }
+      });
+      setRepositories(repositoriesUpdated);
   }
 
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+        title: `${Date.now()}`,
+        url: 'preact.com.br',
+        techs: ["React Native", "Node.js"]
+    });
+    const repository = response.data;
+    setRepositories([ ... repositories, repository]);
+  }
+  
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
+          <FlatList
+              data={repositories}
+              keyExtractor={repository => repository.id}
+              renderItem={({ item: repository }) => (
+                <View style={styles.repositoryContainer}>
+                  <Text style={styles.repository}>{repository.title}</Text>
+                  <View style={styles.techsContainer}>
+                      {repository.techs.map(tech => (
+                          <Text key={tech} style={styles.tech}>{tech}</Text>
+                      ))}                      
+                  </View>
+                  <View style={styles.likesContainer}>
+                      <Text style={styles.likeText} testID={`repository-likes-${repository.id}`}>
+                          {repository.likes} curtidas  
+                      </Text>
+                  </View>
+                  <TouchableOpacity style={styles.button} 
+                  onPress={() => handleLikeRepository(repository.id)} testID={`like-button-${repository.id}`}>
+                        <Text style={styles.buttonText}>Curtir</Text>
+                  </TouchableOpacity>
+                </View>                
+              )}
+          />
+          <TouchableOpacity activeOpacity={0.6} style={styles.buttonAdd} onPress={() => handleAddRepository()}>
+              <Text style={styles.buttonAddText}>Adicionar projeto</Text>
           </TouchableOpacity>
-        </View>
       </SafeAreaView>
     </>
   );
@@ -104,4 +120,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#7159c1",
     padding: 15,
   },
+  buttonAdd: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonAddText: {
+      fontWeight: 'bold',
+      fontSize: 16,
+  }
 });
